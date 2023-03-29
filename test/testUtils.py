4b3c800e68d418c8521cede331b4e9d1b3e2c890
Bastianleaf <bastian.martinez@ing.uchile.cl>
2023-03-29 18:30:21 -0300

    Add update_file_by_tuples function

--- test/testUtils.py
+++ test/testUtils.py
@@ -1,21 +1,29 @@
+import csv
+import os
 from unittest import TestCase
-from utils import valid_date, get_date_list_between_two_given_dates
+from utils import (
+    valid_date,
+    get_date_list_between_two_given_dates,
+    update_file_by_tuples,
+)
 import datetime
 import argparse
 
-class UtilsTest(TestCase):
 
+class TestValidDate(TestCase):
     def test_valid_date(self):
-        date: str = '20101010'
-        expected_date: datetime = datetime.strptime(date, "%Y%m%d")
+        date: str = "2010-10-10"
+        expected_date: datetime = datetime.datetime.strptime(date, "%Y-%m-%d")
         # valid date
         self.assertEqual(expected_date, valid_date(date))
         # not valid date
         with self.assertRaises(argparse.ArgumentTypeError):
-            valid_date('1')
+            valid_date("1")
         with self.assertRaises(argparse.ArgumentTypeError):
-            valid_date('2010-01-01')
-        
+            valid_date("20100101")
+
+
+class TestGetDateList(TestCase):
     def test_get_date_list_between_two_given_dates(self):
         start_date: datetime.date = datetime.date(2022, 1, 1)
         end_date: datetime.date = datetime.date(2022, 1, 10)
@@ -29,22 +37,66 @@ class UtilsTest(TestCase):
             datetime.date(2022, 1, 7),
             datetime.date(2022, 1, 8),
             datetime.date(2022, 1, 9),
-            datetime.date(2022, 1, 10)
+            datetime.date(2022, 1, 10),
         ]
-        self.assertEqual(expected_date_list, get_date_list_between_two_given_dates(start_date, end_date))
+        self.assertEqual(
+            expected_date_list,
+            get_date_list_between_two_given_dates(start_date, end_date),
+        )
 
     def test_get_date_list_between_two_given_dates_same_day_case(self):
         start_date: datetime.date = datetime.date(2022, 1, 1)
         end_date: datetime.date = datetime.date(2022, 1, 1)
-        expected_date_list: list[datetime.date] = [
-            datetime.date(2022, 1, 1)
-        ]
-        self.assertEqual(expected_date_list, get_date_list_between_two_given_dates(start_date, end_date))
+        expected_date_list: list[datetime.date] = [datetime.date(2022, 1, 1)]
+        self.assertEqual(
+            expected_date_list,
+            get_date_list_between_two_given_dates(start_date, end_date),
+        )
 
     def test_get_date_list_between_two_given_dates_same_day_case(self):
         start_date: datetime.date = datetime.date(2022, 1, 1)
         end_date: datetime.date = datetime.date(2019, 1, 1)
 
         with self.assertRaises(ValueError):
-                    get_date_list_between_two_given_dates(start_date, end_date)
+            get_date_list_between_two_given_dates(start_date, end_date)
 
+
+class TestUpdateFileByTuples(TestCase):
+    def setUp(self) -> None:
+        self.file_name_list: list[str] = [
+            "2021-06-30.bip",
+            "2021-06-30.bip.gz",
+            "2021-06-30.bip.zip",
+        ]
+        return super().setUp()
+
+    def test_update_file_by_tuples(self):
+        input_file: str = os.path.join(os.path.dirname(__file__), "files", self.file_name_list[0]) 
+        output_file: str = input_file + "-updated"
+        tuple_1: tuple = ('3', '2', '1')
+        update_file_by_tuples(input_file, output_file, [tuple_1])
+        with open(input_file, 'r') as input, open(output_file, 'r') as output:
+            reader_input: csv.reader = csv.reader(input, delimiter='|')
+            reader_output: csv.reader = csv.reader(output, delimiter='|')
+            for row_input, row_output in zip(reader_input, reader_output):
+                if row_input[3] == '2':
+                    self.assertEqual(row_output[3], '1')
+        os.remove(output_file)
+    
+    def test_update_file_by_tuples(self):
+        input_file: str = os.path.join(os.path.dirname(__file__), "files", self.file_name_list[0]) 
+        output_file: str = input_file + "-updated"
+        tuple_list: list[tuple] = [
+            ('3', '2', '1'),
+            ('3', '5', '1')
+        ]
+        update_file_by_tuples(input_file, output_file, tuple_list)
+        with open(input_file, 'r') as input, open(output_file, 'r') as output:
+            reader_input: csv.reader = csv.reader(input, delimiter='|')
+            reader_output: csv.reader = csv.reader(output, delimiter='|')
+            for row_input, row_output in zip(reader_input, reader_output):
+                if row_input[3] == '2':
+                    self.assertEqual(row_output[3], '1')
+                if row_input[3] == '5':
+                    self.assertEqual(row_output[3], '1')
+        os.remove(output_file)
--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 import argparse
-
+import csv
 
 def valid_date(s):
     try:
-        return datetime.strptime(s, "%Y%m%d")
+        return datetime.strptime(s, "%Y-%m-%d")
     except ValueError:
         msg: str = "Not a valid date: '{0}'.".format(s)
         raise argparse.ArgumentTypeError(msg)
@@ -19,3 +19,14 @@ def get_date_list_between_two_given_dates(start_date: datetime, end_date: dateti
         dates.append(start_date)
         start_date += delta
     return dates
+
+def update_file_by_tuples(input_file_name: str, output_file_name: str, tuples: list, delimiter="|"):
+    with open(input_file_name, 'r') as input_file, open(output_file_name, 'w') as output_file:
+        reader: csv.reader = csv.reader(input_file, delimiter=delimiter)
+        writer: csv.writer = csv.writer(output_file, delimiter=delimiter)
+        for row in reader:
+            for tuple_list in tuples:
+                if int(tuple_list[0]) < len(row) and row[int(tuple_list[0])] == str(tuple_list[1]):
+                    row[int(tuple_list[0])] = tuple_list[2]
+                    
+            writer.writerow(row)
